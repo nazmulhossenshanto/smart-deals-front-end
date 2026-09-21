c1a34801d6a36ae3b6c3d7f37bf24efc0d38070b
nazmulhossenshanto <https://github.com/nazmulhossenshanto>
2026-09-21 16:36:58 +0600

    added firebaseConfig keys into .env file

--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -6,13 +6,14 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBawe_CpYjvYVIL7UijwI2--gv8dYt-sLw",
-  authDomain: "smart-deals-5b8f2.firebaseapp.com",
-  projectId: "smart-deals-5b8f2",
-  storageBucket: "smart-deals-5b8f2.firebasestorage.app",
-  messagingSenderId: "69019342683",
-  appId: "1:69019342683:web:851293284a038b3428ac7e"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
